ea53be0cb23cebea875cad8fb97894c07f31766a
BLACKMOUNTAIN\pshakotko <pshakotko@allvuesystems.com>
2022-02-10 17:55:41 +0200

    added Command service and test endpoint

---
+++ src/WebApi/Controllers/PlatformController.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandService.WebApi.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("command/[controller]")]
+    public class PlatformController : ControllerBase
+    {
+        public PlatformController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            Console.WriteLine("--------------->");
+            return Ok();
+        }
+    }
+}
